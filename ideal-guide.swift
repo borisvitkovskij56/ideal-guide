bd20c5b78e8578b66d3bc63a52fb6176cb99bf3d
Ruth047 <151012116+borisvitkovskij56@users.noreply.github.com>
2023-12-15 02:18:43 +0700

    Create ideal-guide.swift

---
+++ ideal-guide.swift
@@ -0,0 +1,6 @@
+    case selectElement(ElementType)
+    case removeSelectedPoints
+    case switchEditMode
+    case changeCornerRadius(CGFloat)
+    case addShape(ShapeData)
+    case changeShape(ShapeModification, id: String)
